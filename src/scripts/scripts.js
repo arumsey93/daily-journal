004c033d6cdd48f29c008e995ce0c3110c567436
Alex Rumsey <alexrumsey1993@gmail.com>
2019-06-05 09:12:07 -0500

    updated for lesson 2

---
+++ src/scripts/scripts.js
@@ -0,0 +1,12 @@
+/*
+    Define the keys and value for a JavaScript object that
+    represents a journal entry about what you learned today
+*/
+const objectsJournalEntry = {
+
+}
+const journalEntry = ["Manipulating the DOM"]
+
+console.log(journalEntry)
+
+journalEntry.push("JS Objects", "Functions and Logic", "Building DOM Components")
