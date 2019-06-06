6982e7fc42998e7f5058e0d0d41b3b95eed1ac06
Alex Rumsey <alexrumsey1993@gmail.com>
2019-06-06 08:59:28 -0500

    did part 3 exercise

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -10,3 +10,39 @@ const journalEntry = ["Manipulating the DOM"]
 console.log(journalEntry)
 
 journalEntry.push("JS Objects", "Functions and Logic", "Building DOM Components")
+
+const journalEntries = [
+    {
+        date: "07/24/2018",
+        concept: "Array methods",
+        entry: "We learned about 4 different array methods today. forEach made sense, but the others still confuse me.",
+        mood: "Ok"
+    },
+    {
+        date: "6/5/2019",
+        concept: "APIs",
+        entry: "Learning to use Postman with APIs, and learning more about objects and the DOM.",
+        mood: "happy"
+    }
+]
+
+const makeJournalEntryComponent = (date, concept, entry, mood) => {
+    return `
+    <p>
+        On ${date} we learned about ${concept} and my notes included that we ${entry} and after learning the concept I feel ${mood}
+        about the content and my ability to apply the methods daily.
+    </p>
+    `
+}
+
+const newEntry = document.querySelector(".entryLog")
+
+for (let i = 0; i < journalEntries.length; i++) {
+    newEntry.innerHTML += makeJournalEntryComponent(
+        journalEntries[i].date,
+        journalEntries[i].concept,
+        journalEntries[i].entry,
+        journalEntries[i].mood
+    );
+}
+
